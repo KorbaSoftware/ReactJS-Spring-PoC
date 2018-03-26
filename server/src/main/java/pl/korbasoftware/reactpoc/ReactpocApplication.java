52af73b38001597270cce6740c2661529d7047fd
kamil <wujeckamil@gmail.com>
2018-03-27 00:05:15 +0200

    init commit

---
+++ server/src/main/java/pl/korbasoftware/reactpoc/ReactpocApplication.java
@@ -0,0 +1,11 @@
+package pl.korbasoftware.reactpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactpocApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ReactpocApplication.class, args);
+	}
+}
